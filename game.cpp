fd2df5a46c6fc57d6b87ab433510c858b3b89284
Hurshal Patel <hpatel516@gmail.com>
2013-02-09 15:42:08 -0800

    changed colors and added keys

--- game.cpp
+++ game.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <GL/glut.h>
+#include <stdio.h>
 
 using namespace std;
 
@@ -10,6 +11,7 @@ int new_cube_delay = 10;
 vector< vector<float> > cubes;
 float init_vel[] = {0.0, 0.0, 1.0};
 vector<float> cur_vel (init_vel, init_vel + sizeof(init_vel) / sizeof(float));
+bool* key_states = new bool[256];
 
 /* Initialize OpenGL Graphics */
 void initGL() {
@@ -19,7 +21,7 @@ void initGL() {
   glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
 }
 
-void draw_cube(float x, float y, float z) {
+void draw_cube(float x, float y, float z, float r, float g, float b) {
   glLoadIdentity();
   glTranslatef(x, y, z);
   glBegin(GL_QUADS);
@@ -27,14 +29,14 @@ void draw_cube(float x, float y, float z) {
   if (y < 0) {
     // Top face (y = 1.0f)
     // Define vertices in counter-clockwise (CCW) order with normal pointing out
-    glColor3f(0.0f, 1.0f, 0.0f);     // Green
+    glColor3f(0.5*r, 0.5*g, 0.5*b);     // Green
     glVertex3f( 1.0f, 1.0f, -1.0f);
     glVertex3f(-1.0f, 1.0f, -1.0f);
     glVertex3f(-1.0f, 1.0f,  1.0f);
     glVertex3f( 1.0f, 1.0f,  1.0f);
   } else if (y > 0) {
     // Bottom face (y = -1.0f)
-    glColor3f(1.0f, 0.5f, 0.0f);     // Orange
+    glColor3f(0.5*r, 0.5*g, 0.5*b);     // Orange
     glVertex3f( 1.0f, -1.0f,  1.0f);
     glVertex3f(-1.0f, -1.0f,  1.0f);
     glVertex3f(-1.0f, -1.0f, -1.0f);
@@ -43,14 +45,14 @@ void draw_cube(float x, float y, float z) {
 
   if (x > 0) {
     // Left face (x = -1.0f)
-    glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+    glColor3f(0.5*r, 0.5*g, 0.5*b);     // Blue
     glVertex3f(-1.0f,  1.0f,  1.0f);
     glVertex3f(-1.0f,  1.0f, -1.0f);
     glVertex3f(-1.0f, -1.0f, -1.0f);
     glVertex3f(-1.0f, -1.0f,  1.0f);
   } else if (x < 0) {
     // Right face (x = 1.0f)
-    glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
+    glColor3f(0.5*r, 0.5*g, 0.5*b);     // Magenta
     glVertex3f(1.0f,  1.0f, -1.0f);
     glVertex3f(1.0f,  1.0f,  1.0f);
     glVertex3f(1.0f, -1.0f,  1.0f);
@@ -58,7 +60,7 @@ void draw_cube(float x, float y, float z) {
   }
 
   // Front face  (z = 1.0f)
-  glColor3f(1.0f, 0.0f, 0.0f);     // Red
+  glColor3f(r, g, b);                   // Red
   glVertex3f( 1.0f,  1.0f, 1.0f);
   glVertex3f(-1.0f,  1.0f, 1.0f);
   glVertex3f(-1.0f, -1.0f, 1.0f);
@@ -73,12 +75,12 @@ void display() {
   int num_to_delete = 0;
   for (int i = 0; i < cubes.size(); i++) {
     float coords[] = {cubes[i][0] + cur_vel[0], cubes[i][1] + cur_vel[1],
-      cubes[i][2] + cur_vel[2]};
+      cubes[i][2] + cur_vel[2], cubes[i][3], cubes[i][4], cubes[i][5]};
     vector<float> new_cube (coords, coords + sizeof(coords) / sizeof(float));
     if (new_cube[2] > 1) {
       num_to_delete++;
     } else {
-      draw_cube(new_cube[0], new_cube[1], new_cube[2]);
+      draw_cube(new_cube[0], new_cube[1], new_cube[2], new_cube[3], new_cube[4], new_cube[5]);
       cubes[i] = new_cube;
     }
   }
@@ -92,16 +94,34 @@ void add_cube(vector< vector<float> > cubes_added) {
   float x = ((float)rand()/(float)RAND_MAX) * 60.0 - 30.0;
   float y = ((float)rand()/(float)RAND_MAX) * 60.0 - 30.0;
   float z = -100.0;
+  float r = ((float)rand()/(float)RAND_MAX);
+  float g = ((float)rand()/(float)RAND_MAX);
+  float b = ((float)rand()/(float)RAND_MAX);
+
   for (int i = 0; i < cubes_added.size(); i++) {
     if (abs(cubes_added[i][0] - x) <= 2 && abs(cubes_added[i][1] - y) <= 2) {
       add_cube(cubes_added);
       return;
     }
   }
-  float coords[] = {x, y, z};
+  float coords[] = {x, y, z, r, g, b};
   vector<float> cube (coords, coords + sizeof(coords) / sizeof(float));
   cubes_added.push_back(cube);
   cubes.insert(cubes.begin(), cube);
+}
+
+void update_speed() {
+  if (key_states['a'])
+    cur_vel[0] = std::min(cur_vel[0] + 0.2, 1.0);
+
+  if (key_states['d'])
+    cur_vel[0] = std::max(cur_vel[0] - 0.2, -1.0);
+
+  if (key_states['s'])
+    cur_vel[1] = std::min(cur_vel[1] + 0.2, 1.0);
+
+  if (key_states['w'])
+    cur_vel[1] = std::max(cur_vel[1] - 0.2, -1.0);
 }
 
 void update_game() {
@@ -111,7 +131,17 @@ void update_game() {
       add_cube(cubes_added);
     }
   }
-  cube_delay_count++;
+  cube_delay_count+=1;
+
+  update_speed();
+}
+
+void key_pressed(unsigned char key, int x, int y) {
+  key_states[key] = true;
+}
+
+void key_up(unsigned char key, int x, int y) {
+  key_states[key] = false;
 }
 
 void timer(int value) {
@@ -136,6 +166,8 @@ int main(int argc, char** argv) {
   glutCreateWindow("Kinect Runner");   // Create window with the given title
   glutDisplayFunc(display);       // Register callback handler for window re-paint event
   glutReshapeFunc(reshape);       // Register callback handler for window re-size event
+  glutKeyboardFunc(key_pressed);  // Register callback handler for key press
+  glutKeyboardUpFunc(key_up);     // Register callback handler for key up
   initGL();                       // Our own OpenGL initialization
   glutTimerFunc(0, timer, 0);
   update_game();
